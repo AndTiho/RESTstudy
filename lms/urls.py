374264c906afb446605b1be8c781738f81ddd7d2
Andrei Tihomirov <moon200505@mail.ru>
2026-01-14 13:17:45 +0300

    done without ** task

--- lms/urls.py
+++ lms/urls.py
@@ -1,13 +1,21 @@
-from lms.apps import LmsConfig
-from rest_framework.routers import  DefaultRouter
+from django.urls import path
 
-from lms.views import CourseViewSet
+from lms.apps import LmsConfig
+from rest_framework.routers import DefaultRouter
+
+from lms.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
+    LessonDestroyAPIView
 
 app_name = LmsConfig.name
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
-urlpatterns =[
+urlpatterns = [
+                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+                  path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+                  path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson'),
+                  path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+                  path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 
-] + router.urls
+              ] + router.urls
