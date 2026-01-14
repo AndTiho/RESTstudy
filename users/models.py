de61731ea756ac30424f6d378256002304a3dd92
Andrei Tihomirov <moon200505@mail.ru>
2026-01-14 16:48:05 +0300

    done with ** task

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     country = models.CharField(max_length=150, blank=True, null=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
