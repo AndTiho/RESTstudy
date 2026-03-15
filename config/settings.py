249e4a9e0a58e32bcce63d984004c13df681a123
Andrei Tihomirov <moon200505@mail.ru>
2026-03-15 12:45:51 +0300

    update model user for migrations

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
+CSRF_TRUSTED_ORIGINS = ["http://*"]
 
 # Application definition
 
